a8cbe2632af647a807fa69b28cafd94c607bfb0a
V-Malar <128116398+V-Malar@users.noreply.github.com>
2023-06-26 18:39:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Register from './components/Register';
+
+function App() {
+  return (
+    <div className="App">
+      <Register/>
+      {/* <Run/> */}
+    </div>
+  );
+}
+
+export default App;
